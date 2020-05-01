23a7c3969cea935d29b3986c6f806fc4782d4fbd
Moosa Hassan <moosa7021@gmail.com>
2020-05-01 20:10:04 +1000

    better formatting and added more content

--- js/scripting.js
+++ js/scripting.js
@@ -2,3 +2,9 @@
 const scroll = new SmoothScroll('.page-content a[href*=""]', {
     speed: 800
 });
+
+document.getElementById("scroll-down").onmousedown = function(event) {
+    if (event.which == 3) {
+        alert("right clicked!");
+    }
+}
